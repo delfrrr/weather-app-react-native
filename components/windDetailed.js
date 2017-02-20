8605520214d628f2bab2f26fb92fb993bddabc15
Vova Bilonenko <bilonenko.v@gmail.com>
2017-02-20 20:11:15 +0100

    change shape of wind viz

--- components/windDetailed.js
+++ components/windDetailed.js
@@ -12,10 +12,8 @@ const connect = require('react-redux').connect;
 let path = React.createFactory(require('react-native-svg').Path);
 const sliceDataPoints = require('../lib/sliceDataPoints');
 const g = React.createFactory(require('react-native-svg').G);
-// const svgText = React.createFactory(require('react-native-svg').Text);
-// const circle = React.createFactory(require('react-native-svg').Circle);
-const {scaleLinear, scaleQuantize} = require('d3-scale');
-const {arc} = require('d3-shape');
+const {scaleLinear, scaleThreshold} = require('d3-scale');
+const d3Path = require('d3-path').path;
 
 /**
  * @param  {Object} props
@@ -70,43 +68,35 @@ module.exports = connect(
         if (!points) {
             return null;
         }
-        let y0 = 50;
-        let l = 15;
+        const y0 = 50;
+        const l = 15;
         const bofort = scaleLinear()
             .domain([0, .3, 1.6, 3.4, 5.5, 8, 10.8, 13.9, 17.2, 20.8])
             .range([0,  1,  2,   3,   4,   5, 6,    7, 8, 9])
         const xScale = scaleLinear()
             .domain([0, 23])
             .range([0, width / 2 - l / 2]);
-        const radius = scaleLinear()
-            .domain([0,  4,    5,    6, Infinity])
-            .range([ 10, 1.81, 1.13, 1, 1]);
-        const size = scaleQuantize()
-            .domain([3,   4,  5,   6])
-            .range([0,   0.5,  1, 1.5]);
+        const size = scaleThreshold()
+            .domain([3, 4,   5,  6,  7, 8])
+            .range([ 0, 0, 0.5, 1,  2,  3, 4, 4]);
         return g(
             {},
             points.map(({hour, speed}) => {
                 let x = xScale(hour);
                 let key = hour;
                 let b = bofort(speed);
-                let r = radius(b);
-                let alpha = Math.asin(1/r);
-                let dx = (l/2) * Math.pow(r * r - 1, .5);
-                let start = Math.PI / 2;
-                let strokeSize = size(b);
-                let d = arc()({
-                    innerRadius: r * (l/2) - strokeSize,
-                    outerRadius: r * (l/2) ,
-                    startAngle: start - alpha,
-                    endAngle: start + alpha
-                });
+                let strokeWidth = size(b);
+                let curve = d3Path();
+                curve.moveTo(x, y0 - l / 2);
+                curve.lineTo(x + l / 3, y0);
+                curve.lineTo(x, y0 + l / 2);
+                let d = curve.toString();
                 return path({
                     key,
-                    fill: 'white',
-                    d,
-                    x: x - dx,
-                    y: y0
+                    stroke: 'white',
+                    strokeWidth: strokeWidth,
+                    fill: 'transparent',
+                    d
                 });
             })
         );
